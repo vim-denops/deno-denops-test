a0020b89ef54f2c462c491992cbd018597baa0e2
Alisue <lambdalisue@gmail.com>
2022-12-31 22:59:01 +0900

    :+1: Do NOT expose unnecessary constants

--- denops_test/mod.ts
+++ denops_test/mod.ts
@@ -5,15 +5,12 @@
  * [deno]: https://deno.land/
  * [denops.vim]: https://github.com/vim-denops/denops.vim
  *
- * By using this module, developers can write denops tests like:
- *
  * ```typescript
  * import {
- *   assert,
  *   assertEquals,
  *   assertFalse,
  * } from "https://deno.land/std@0.170.0/testing/asserts.ts";
- * import { test } from "./tester.ts";
+ * import { test } from "./mod.ts";
  *
  * test(
  *   "vim",
@@ -30,32 +27,9 @@
  *     assert(await denops.call("has", "nvim"));
  *   },
  * });
- *
- * test({
- *   mode: "any",
- *   name: "test(mode:any) start vim or nvim to test denops features",
- *   fn: async (denops) => {
- *     assertEquals(
- *       await denops.call("range", 10),
- *       [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
- *     );
- *   },
- * });
- *
- * test({
- *   mode: "all",
- *   name: "test(mode:all) start both vim and nvim to test denops features",
- *   fn: async (denops) => {
- *     assertEquals(
- *       await denops.call("range", 10),
- *       [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
- *     );
- *   },
- * });
  * ```
  * @module
  */
-export * from "./conf.ts";
 export type { TestDefinition } from "./tester.ts";
 export type { WithDenopsOptions } from "./with.ts";
 export { test } from "./tester.ts";
